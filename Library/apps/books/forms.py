d71d47ccb8fd991c61d2d5afcaa8e277f66cbb31
leoRock037 <clbenitez64@gmail.com>
2014-05-28 02:14:47 -0500

    repositorio con cambios para despliegue

---
+++ Library/apps/books/forms.py
@@ -0,0 +1,69 @@
+
+from django import forms
+from apps.books.models import Author,Publisher,CategoryBook,Book
+
+
+class LoginForm(forms.Form):
+	username = forms.CharField(widget = forms.TextInput())
+	password = forms.CharField(widget = forms.PasswordInput(render_value = False))
+
+
+class addBookForm(forms.ModelForm):
+	class Meta:
+		model 	= Book
+		exclude ={'status','frontbook',}
+
+
+
+"""
+class addBookForm(forms.Form):
+	title 				= forms.CharField(max_length = 100)
+	authors 			= forms.ModelMultipleChoiceField(queryset=Author.objects.all())
+	ISBN 				= forms.CharField(max_length = 13)
+	publisher 			= forms.ModelChoiceField(queryset=Publisher.objects.all())
+	publication_date 	= forms.DateField()
+	price 				= forms.DecimalField(required=False)
+	description 		= forms.CharField(widget = forms.Textarea) 			
+	category 			= forms.ModelMultipleChoiceField(queryset=CategoryBook.objects.all())
+	frontbook 			= forms.ImageField(required=False)
+
+	def clean(self):#sirve para validar la informacion ingresada por el usuario
+		return self.cleaned_data
+"""
+
+
+
+class addNewsForm(forms.Form):
+	title 		= forms.CharField(max_length=50)
+	newsImage	= forms.ImageField(required=False)#cambiar para poder agregarla
+	date 		= forms.DateField()
+	description = forms.CharField(widget = forms.Textarea)
+
+	def clean(self):#sirve para validar la informacion ingresada por el usuario
+		return self.cleaned_data
+
+class addPublisherForm(forms.Form):
+	name 			= forms.CharField(max_length = 30)
+	address 		= forms.CharField(max_length = 50)
+	city 			= forms.CharField(max_length = 60)
+	state_province 	= forms.CharField(max_length = 50)
+	website 		= forms.URLField(max_length=200)
+	logotype 		= forms.ImageField(required=False)#cambiar....
+	
+
+	def clean(self):#sirve para validar la informacion ingresada por el usuario
+		return self.cleaned_data
+
+
+class addAuthorForm(forms.Form):
+	first_name 		= forms.CharField(max_length = 30)
+	last_name 		= forms.CharField(max_length = 40)
+	email 			= forms.EmailField()
+	biography 		= forms.CharField(widget = forms.Textarea)
+	photo 			= forms.ImageField(required=False)#cmabir.........
+	
+	
+
+	def clean(self):#sirve para validar la informacion ingresada por el usuario
+		return self.cleaned_data
+	
