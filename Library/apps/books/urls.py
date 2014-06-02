5e98d7d6dc8977a05848148db48dc7f74d5440fc
leoRock037 <clbenitez64@gmail.com>
2014-06-02 00:56:23 -0500

    agregadas views para eliminar en mehu admin

--- Library/apps/books/urls.py
+++ Library/apps/books/urls.py
@@ -6,33 +6,69 @@ from apps.books import views
 urlpatterns = patterns('apps.books.views',
 	
 	
-	
 	url(r'^home/$','home', name = 'view_home'),
+
+	url(r'^sesion-admin/$','sesion_admin_view',name = 'view_sesion'),
+
+
+	url(r'^contact/$','contact', name = 'view_contact'),
+
+
+#.........................user-createUser...............................................
+
+	url(r'^createuser/user$', 'createUser' ,name='createUser'),
+    url(r'^createuser/$', 'user' ,name='user'),
+
+
+#.........................books.........................................................
 
 	url(r'^books/$','index_books', name = 'view_books'),
 	url(r'^books/book/(?P<id_book>\d+)$','book', name ='view_book'),
+	url(r'^categoryBook/$','category_book', name = 'view_CategoryBook'),
+
+	url(r'^addBook/$','addBook_view',name = 'view_addBook'),
+
+
+#.........................author.......................................................
 
 	url(r'^authors/$','index_authors', name = 'view_authors'),
 	url(r'^authors/author/(?P<id_author>\d+)$','author', name = 'view_author'),
 
-	url(r'^publishers/$','index_publishers', name = 'view_publishers'),
-	url(r'^publishers/publisher/(?P<id_pub>\d+)$','publisher', name ='view_publisher'),
-	
-	url(r'^news/$','news', name = 'view_news'),
-	url(r'^contact/$','contact', name = 'view_contact'),
-	url(r'^categoryBook/$','category_book', name = 'view_CategoryBook'),
-	
-	url(r'^addBook/$','addBook_view',name = 'view_addBook'),
-	url(r'^addNews/$','addNews_view',name = 'view_addNews'),
-	url(r'^addPublisher/$','addPublisher_view',name = 'view_addPublisher'),
 	url(r'^addAuthor/$','addAuthor_view',name = 'view_addAuthor'),
 
 	url(r'^editAuthor/(?P<id_author>.*)/$','edit_author_view',name= "view_edit_author"),
 	url(r'^listauthors/$','index_authors_edit', name = 'view_authorslist'),
 
+	url(r'^deleteAuthor/(?P<id_author>.*)$', 'author_delete', name='view_delete_author'),
+	url(r'^authors_delete/$','index_authors_delete', name = 'view_authorslist_delete'),
 
-	url(r'^createuser/user$', 'createUser' ,name='createUser'),
-    url(r'^createuser/$', 'user' ,name='user'),
+
+#.........................publisher...................................................
+
+	url(r'^publishers/$','index_publishers', name = 'view_publishers'),
+	url(r'^publishers/publisher/(?P<id_pub>\d+)$','publisher', name ='view_publisher'),
+
+	url(r'^addPublisher/$','addPublisher_view',name = 'view_addPublisher'),
+
+	url(r'^deletePublisher/(?P<id_publisher>.*)$', 'publisher_delete', name='view_delete_publisher'),
+	url(r'^publisher_delete/$','index_publishers_delete', name = 'view_publisherslist_delete'),
+
+
+#.........................news.......................................................
 	
-	url(r'^sesion-admin/$','sesion_admin_view',name = 'view_sesion'),
+	url(r'^news/$','news', name = 'view_news'),
+	url(r'^addNews/$','addNews_view',name = 'view_addNews'),
+	
+
+
+	
+	
+	
+
+	
+	
+
+	
+	
+
 )
--- Library/apps/books/views.py
+++ Library/apps/books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, render_to_response, get_object_or_404
+from django.shortcuts import render, render_to_response, get_object_or_404,redirect
 from django.template import RequestContext
 from apps.books.models import CategoryBook, News, Publisher, Author,Book,Contact
 from apps.books.forms import addNewsForm,addPublisherForm,addAuthorForm,addBookForm
@@ -16,6 +16,16 @@ def home(request):
 	return render_to_response('home.html',ctx,context_instance = RequestContext(request))
 
 
+
+def sesion_admin_view(request):
+	return render_to_response('sesion-admin.html')
+
+
+# .......................................Books......................................................................
+
+def category_book(request):
+	return render_to_response('CategoryBook.html')
+
 def index_books(request):
 	book = Book.objects.filter(status = True)
 	cat = CategoryBook.objects.filter(status = True)
@@ -27,58 +37,6 @@ def book(request,id_book):
 	book = Book.objects.filter(status = True)
 	cat = CategoryBook.objects.filter(status = True)
 	return render_to_response('book.html',{'book':book_id,'books':book,"category":cat},context_instance = RequestContext(request))
-
-def category_book(request):
-	return render_to_response('CategoryBook.html')
-	
-
-
-def index_authors(request):
-	auth = Author.objects.filter(status = True)
-	ctx  = {"authors":auth} 
-	return render_to_response('index_authors.html',ctx,context_instance = RequestContext(request))
-
-def index_authors_edit(request):
-	auth = Author.objects.filter(status = True)
-	ctx  = {"authors":auth} 
-	return render_to_response('index_authors_edit.html',ctx,context_instance = RequestContext(request))
-
-def author(request, id_author):
-	auth_id = get_object_or_404(Author,pk=id_author)#
-	auth = Author.objects.filter(status = True)
-	return render_to_response('author.html',{'author':auth_id,'authors':auth},context_instance = RequestContext(request))
-
-
-
-
-def index_publishers(request):
-	pub = Publisher.objects.filter(status = True)
-	ctx = {"pub":pub}
-	return render_to_response('index_publishers.html',ctx,context_instance = RequestContext(request))
-
-def publisher(request,id_pub):
-	pub_id = get_object_or_404(Publisher,pk=id_pub)
-	pub = Publisher.objects.filter(status = True)
-	return render_to_response('publisher.html',{'pub_id':pub_id,'pub':pub},context_instance = RequestContext(request))
-	
-
-def news(request):
-	news = News.objects.filter(status = True)
-	ctx = {"News":news}
-	return render_to_response('news.html',ctx,context_instance = RequestContext(request))
-
-
-
-def contact(request):
-	cont = Contact.objects.filter(status=True)
-	ctx = {"Contact":cont}
-	return render_to_response('contact.html',ctx,context_instance = RequestContext(request))
-
-
-
-def sesion_admin_view(request):
-	return render_to_response('sesion-admin.html')
-
 
 @login_required()
 def addBook_view(request):
@@ -103,6 +61,61 @@ def addBook_view(request):
 	return render_to_response('addBook.html',ctx,context_instance=RequestContext(request))
 
 
+
+
+# ...........................Authors......................................................................
+
+
+def author(request, id_author):
+	auth_id = get_object_or_404(Author,pk=id_author)#
+	auth = Author.objects.filter(status = True)
+	return render_to_response('author.html',{'author':auth_id,'authors':auth},context_instance = RequestContext(request))
+
+def index_authors(request):
+	auth = Author.objects.filter(status = True)
+	ctx  = {"authors":auth} 
+	return render_to_response('index_authors.html',ctx,context_instance = RequestContext(request))
+
+@login_required()
+def index_authors_edit(request):
+	auth = Author.objects.filter(status = True)
+	ctx  = {"authors":auth} 
+	return render_to_response('index_authors_edit.html',ctx,context_instance = RequestContext(request))
+
+@login_required()
+def addAuthor_view(request):
+	info = "Loading information...."
+	if request.method == "POST":
+		form = addAuthorForm(request.POST,request.FILES)
+		if form.is_valid():
+			newimg = Author(photo = request.FILES['photo'])
+			add = form.save(commit=False)
+			add.status = True
+			add.save()#guarda la info
+			info ="saved information!"
+			return HttpResponseRedirect(reverse('apps.books.views.index_authors'))		
+	else:
+		form = addAuthorForm()
+	image=Author.objects.all()
+	ctx = {'form':form, 'information':info,'image':image}
+	return render_to_response('addAuthor.html',ctx,context_instance=RequestContext(request))
+
+
+@login_required()
+def index_authors_delete(request):
+	auth = Author.objects.filter(status = True)
+	ctx  = {"authors":auth} 
+	return render_to_response('index_authors_delete.html',ctx,context_instance = RequestContext(request))
+
+
+@login_required()
+def author_delete(request, id_author, template_name='deleteAuthor.html'):
+    author = get_object_or_404(Author, pk=id_author)    
+    if request.method=='POST':
+        author.delete()
+        return redirect('view_authorslist_delete')
+    return render(request, template_name, {'object':author})
+
 @login_required()
 def edit_author_view(request,id_author):
 	info = "Loading information...."
@@ -120,6 +133,67 @@ def edit_author_view(request,id_author):
 		form = addAuthorForm(instance=author)
 	ctx = {'form':form,'information':info}
 	return render_to_response('editAuthor.html',ctx,context_instance=RequestContext(request))
+
+
+
+
+
+# ..........................................Publisher.....................................................................
+
+def index_publishers(request):
+	pub = Publisher.objects.filter(status = True)
+	ctx = {"pub":pub}
+	return render_to_response('index_publishers.html',ctx,context_instance = RequestContext(request))
+
+def publisher(request,id_pub):
+	pub_id = get_object_or_404(Publisher,pk=id_pub)
+	pub = Publisher.objects.filter(status = True)
+	return render_to_response('publisher.html',{'pub_id':pub_id,'pub':pub},context_instance = RequestContext(request))
+
+
+@login_required()
+def addPublisher_view(request):
+	info = "Loading information...."
+	if request.method == "POST":
+		form = addPublisherForm(request.POST,request.FILES)
+		if form.is_valid():
+			newimg = Publisher(logotype = request.FILES['logotype'])
+			add = form.save(commit=False)
+			add.status = True
+			add.save()#guarda la info
+			info ="saved information!"
+			return HttpResponseRedirect(reverse('apps.books.views.index_publishers'))		
+	else:
+		form = addPublisherForm()
+	image=Publisher.objects.all()
+	ctx = {'form':form, 'information':info,'image':image}
+	return render_to_response('addPublisher.html',ctx,context_instance=RequestContext(request))
+	
+
+
+@login_required()
+def index_publishers_delete(request):
+	pub = Publisher.objects.filter(status = True)
+	ctx  = {"publishers":pub} 
+	return render_to_response('index_publishers_delete.html',ctx,context_instance = RequestContext(request))
+
+
+@login_required()
+def publisher_delete(request, id_publisher, template_name='deletePublisher.html'):
+    publisher = get_object_or_404(Publisher, pk=id_publisher)    
+    if request.method=='POST':
+        publisher.delete()
+        return redirect('view_publisherslist_delete')
+    return render(request, template_name, {'object':publisher})
+
+
+
+# ..............................................News......................................................................
+
+def news(request):
+	news = News.objects.filter(status = True)
+	ctx = {"News":news}
+	return render_to_response('news.html',ctx,context_instance = RequestContext(request))
 
 
 @login_required()
@@ -141,42 +215,8 @@ def addNews_view(request):
 	return render_to_response('addNews.html',ctx,context_instance=RequestContext(request))
 
 
-@login_required()
-def addPublisher_view(request):
-	info = "Loading information...."
-	if request.method == "POST":
-		form = addPublisherForm(request.POST,request.FILES)
-		if form.is_valid():
-			newimg = Publisher(logotype = request.FILES['logotype'])
-			add = form.save(commit=False)
-			add.status = True
-			add.save()#guarda la info
-			info ="saved information!"
-			return HttpResponseRedirect(reverse('apps.books.views.index_publishers'))		
-	else:
-		form = addPublisherForm()
-	image=Publisher.objects.all()
-	ctx = {'form':form, 'information':info,'image':image}
-	return render_to_response('addPublisher.html',ctx,context_instance=RequestContext(request))
 
-
-@login_required()
-def addAuthor_view(request):
-	info = "Loading information...."
-	if request.method == "POST":
-		form = addAuthorForm(request.POST,request.FILES)
-		if form.is_valid():
-			newimg = Author(photo = request.FILES['photo'])
-			add = form.save(commit=False)
-			add.status = True
-			add.save()#guarda la info
-			info ="saved information!"
-			return HttpResponseRedirect(reverse('apps.books.views.index_authors'))		
-	else:
-		form = addAuthorForm()
-	image=Author.objects.all()
-	ctx = {'form':form, 'information':info,'image':image}
-	return render_to_response('addAuthor.html',ctx,context_instance=RequestContext(request))
+# .............................................User-Create-User......................................................................
 
 
 def user(request):
@@ -189,3 +229,10 @@ def createUser(request):
 	user = User.objects.create_user(username, email, password)	
 	user.save()
 	return HttpResponseRedirect(reverse('django.contrib.auth.views.login'))
+
+# .............................................Contact......................................................................
+
+def contact(request):
+	cont = Contact.objects.filter(status=True)
+	ctx = {"Contact":cont}
+	return render_to_response('contact.html',ctx,context_instance = RequestContext(request))
